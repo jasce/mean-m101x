29b78b5fc70381e138ecc1a36a39f2cb5c94cc83
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-09-01 16:40:44 +0530

    Chapter1 Assignment

---
+++ chapter1/interface.js
@@ -0,0 +1,27 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+
+  db.collection('movies').insert(doc , function (error , result){
+
+  callback(error);
+  });
+
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+
+
+  db.collection('movies').find({ director: director}).sort({'title': 1}).toArray(function(error , result){
+  callback(error, result );
+});
+
+
+};
