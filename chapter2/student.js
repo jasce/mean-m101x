d554d62af09010c426f4ca1afe859230cb92e2a8
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-09-02 13:59:53 +0530

    Added chapter 2

---
+++ chapter2/student.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  courses: [{ type: String, ref: 'Course' }]
+});
+
+/* Returns the student's first name, which we will define
+ * to be everything up to the first space in the student's name.
+ * For instance, "William Bruce Bailey" -> "William" */
+schema.virtual('firstName').get(function() {
+  first = this.name.split(" ");
+  return first[0];
+});
+
+/* Returns the student's last name, which we will define
+ * to be everything after the last space in the student's name.
+ * For instance, "William Bruce Bailey" -> "Bailey" */
+schema.virtual('lastName').get(function() {
+	last = this.name.split(" ");
+  return last[(last.length) - 1];
+});
+
+module.exports = schema;
